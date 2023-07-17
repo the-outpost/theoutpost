588de1675f1689e688872618b5769d2e9fcfb7d4
Nick Hallahan <nick@theoutpost.io>
2023-07-17 15:45:13 -0700

    Install prettier.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The Outpost';
-export const SITE_DESCRIPTION = 'Surveying and mapping with drones.';
+export const SITE_TITLE = 'The Outpost'
+export const SITE_DESCRIPTION = 'Surveying and mapping with drones.'
--- src/content/places/angelcomb/index.md
+++ src/content/places/angelcomb/index.md
@@ -3,5 +3,5 @@ name: 'Angelcomb Mountain'
 fullName: 'Anglecomb Mountain, Tombstone Territorial Park, Yukon, Canada'
 description: 'Lorem ipsum dolor sit amet'
 surveyDate: '2023-06-06'
-lonLat: [0,0]
+lonLat: [0, 0]
 ---
