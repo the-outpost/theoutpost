f441ea63dd103a01e0dd8e86d77402ba8c982685
Nick Hallahan <nick@theoutpost.io>
2023-07-28 15:21:40 -0700

    Complete default map setting.

--- src/js/place.js
+++ src/js/place.js
@@ -1,13 +1,17 @@
 import queryString from "query-string";
 
-export function getMapType(sceneId, _tileLayerId) {
+export function getMapType(defaultMap, sceneId, _tileLayerId) {
     const query = queryString.parse(window.location.hash)
     let mapType = query.map
     if (!mapType) {
-      if (sceneId) {
-        mapType = '3d'
+      if (defaultMap) {
+        mapType = defaultMap.toLowerCase()
       } else {
-        mapType = '2d'
+        if (sceneId) {
+          mapType = '3d'
+        } else {
+          mapType = '2d'
+        }
       }
     }
     return mapType
